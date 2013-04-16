57e287038c06986da35f71ca05aa175fec412b11
Gosha Tugai <gosha.tugai@gmail.com>
2013-04-17 01:40:10 +0930

    Plugin Utils plugin, Regex blacklist

--- MegaHAL/Plugin/Regex.pm
+++ MegaHAL/Plugin/Regex.pm
@@ -1,11 +1,12 @@
 package MegaHAL::Plugin::Regex;
+use Text::Glob qw(glob_to_regex);
 use Safe;
 use utf8;
 use feature 'switch';
 
 sub new {
     my ($class, $serv) = @_;
-    my $self = { 'chans' => {}, 'lastmsg' => {} };
+    my $self = { 'chans' => {}, 'lastmsg' => {}, 'bl' => {} };
     $serv->reg_cb(
         'publicmsg' => sub {
             my ($this,  $nick, $ircmsg) = @_;
@@ -15,7 +16,7 @@ sub new {
             my $message = $ircmsg->{'params'}->[1];
             my $mstr    = join '', keys %{$modes};
             return if $command ne 'PRIVMSG' or $this->is_my_nick($nick);
-            if ($self->{'chans'}->{$chan}) {
+            if ($self->{'chans'}->{lc($chan)} && !(hmatch($self->{'bl'},$nick,$ident))) {
                 if ($message =~ /^ps\/.*\/.*\/[ige]*$/) {
                     my ($B, $C, $U, $O, $V) = ("\cB", "\cC", "\c_", "\cO", "\cV");
                     $message =~ s/\\\\/\x{FFFE}/g;
@@ -63,14 +64,48 @@ sub new {
     return bless $self, $class;
 }
 
+sub hmatch {
+	my ($ref, $nick, $mask)=@_;
+	my $mstr=$nick.'!'.$mask;
+	foreach (@$ref) {
+		$_=new MegaHAL::Regex::CGlobPat($_) if not ref $_;
+		return 1 if $mstr=~$_;
+	}
+	return 0;
+}
+
 sub load {
     my ($self, $data) = @_;
-    $self->{'chans'} = $data;
+    if (ref $data eq 'ARRAY') {
+		$self->{'chans'} = $data->[0];
+		$self->{'bl'} = $data->[1]->{'bl'};
+	}else{
+		$self->{'chans'} = $data;
+	}
 }
 
 sub save {
     my ($self) = @_;
-    return $self->{'chans'};
+    return [$self->{'chans'},{'bl' => [map {ref $_ ? $_->str : $_} @{$self->{'bl'}}]}];
 }
+
+1;
+
+package MegaHAL::Regex::CGlobPat;
+use Text::Glob qw(glob_to_regex);
+
+use overload
+	'""' => sub {$_[0]->[0]},
+	'qr' => sub {$_[0]->[1]},
+	'0+' => sub {0+$_[0]->[0]},
+	'bool' => sub {$_[0]->[0]};
+
+sub new {
+	my ($class,$str,$re)=@_;
+	return bless $class, [$str, $re || glob_to_regex($str)];
+}
+
+sub str {$_[0]->[0]}
+sub re {$_[0]->[1]}
 
 1;
